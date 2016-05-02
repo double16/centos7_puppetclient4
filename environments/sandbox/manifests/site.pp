e0e5addb8144f9f9aba7998abf8369696883879e
fortunecookiezen <phillips.james@gmail.com>
2016-05-02 10:40:30 -0500

    updated ruby inclusions in site.pp

--- environments/sandbox/manifests/site.pp
+++ environments/sandbox/manifests/site.pp
@@ -16,4 +16,22 @@ node 'default' {
 		group => 'root',
 		source => "file:///vagrant/files/etc/sudoers",
 	}
+
+	#wrap this up in a custom module later
+	package { 'rspec-puppet':
+		ensure => 'latest',
+		provider => 'gem',
+	}
+	package { 'puppet-lint':
+		ensure => 'latest',
+		provider => 'gem',
+	}
+
+	# local package for module testing
+	package { 'mariadb-devel' :
+		ensure => 'installed',
+	}
+	package { 'mariadb-libs' :
+		ensure => 'installed',
+	}
 }
